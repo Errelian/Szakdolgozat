f4705614aa5421d39c6bc71b2710c0c73d4a011b
Errelian <55238385+Errelian@users.noreply.github.com>
2022-01-10 12:27:53 +0100

    Fixed SiteUser REST.

--- src/main/java/com/egyetem/szakdolgozat/user/controller/SiteUserController.java
+++ src/main/java/com/egyetem/szakdolgozat/user/controller/SiteUserController.java
@@ -4,84 +4,54 @@ import com.egyetem.szakdolgozat.region.persistance.RegionRepository;
 import com.egyetem.szakdolgozat.regionalAccount.persistance.RegionalAccount;
 import com.egyetem.szakdolgozat.team.persistance.Team;
 import com.egyetem.szakdolgozat.team.persistance.TeamRepository;
-import com.egyetem.szakdolgozat.tournament.persistance.Tournament;
 import com.egyetem.szakdolgozat.tournament.persistance.TournamentRepository;
 import com.egyetem.szakdolgozat.tournamentToTeams.persistance.TournamentToTeamsRepository;
 import com.egyetem.szakdolgozat.user.persistance.SiteUser;
+import com.egyetem.szakdolgozat.user.persistance.SiteUserPasswordChangerPojo;
 import com.egyetem.szakdolgozat.user.persistance.SiteUserRegisterer;
 import com.egyetem.szakdolgozat.user.persistance.SiteUserRepository;
 import com.egyetem.szakdolgozat.user.persistance.SiteUserUsernameChanger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @RestController
 public class SiteUserController {
 
     SiteUserRepository siteUserRepository;
-    RegionRepository regionRepository;
     TeamRepository teamRepository;
-    TournamentRepository tournamentRepository;
-    TournamentToTeamsRepository tournamentToTeamsRepository;
 
     @Autowired
-    public SiteUserController(SiteUserRepository siteUserRepository, RegionRepository regionRepository, TeamRepository teamRepository, TournamentRepository tournamentRepository, TournamentToTeamsRepository tournamentToTeamsRepository){
+    public SiteUserController(SiteUserRepository siteUserRepository, TeamRepository teamRepository) {
         this.siteUserRepository = siteUserRepository;
-        this.regionRepository = regionRepository;
         this.teamRepository = teamRepository;
-        this.tournamentRepository = tournamentRepository;
-        this.tournamentToTeamsRepository = tournamentToTeamsRepository;
-    }
-
-    @RequestMapping("/test1")
-    public String testFindAll(){
-
-        //userRepository.save(new User("SaveTestPista", "someKindOfHash", "saving@save.com"));
-
-        /*List<User> userList = userRepository.findAll();
-        String test = "";
-
-        for (User currUser : userList){
-
-            Set<Team> userTeam =  currUser.getUserTeams();
-
-            for (Team currTeam : userTeam){
-                test = test + currTeam.getTeamName() + "\n";
-            }
-        }*/
-
-        //List<TournamentToTeams> tournamentList = tournamentToTeamsRepository.findAll();
-
-        List<Tournament> tournamentList = tournamentRepository.findAll();
-
-        return tournamentList.get(0).getTeams().toString();
     }
 
     @GetMapping(value = "/users/{userId}") //TODO REMOVE PASSWORD FROM THIS IN A PRETTY WAY
-    public SiteUser getSiteUserInfo(@PathVariable Long userId){
+    public SiteUser getSiteUserInfo(@PathVariable Long userId) {
         return siteUserRepository.findUserById(userId);
     }
 
     @GetMapping(value = "/users/{userId}/accounts")
-    public Set<RegionalAccount> getUsersAccounts(@PathVariable Long userId){
+    public Set<RegionalAccount> getUsersAccounts(@PathVariable Long userId) {
         return siteUserRepository.findUserById(userId).getRegionalAccounts();
     }
 
     @GetMapping(value = "/users/{userId}/teams")
-    public Set<Team> getUserTeams(@PathVariable Long userId){
+    public Set<Team> getUserTeams(@PathVariable Long userId) {
         return teamRepository.findByTeamMembers_Id(userId);
     }
 
     @PutMapping(value = "/users/changeName", consumes = "application/json")
-    public void changeUsername(@RequestBody SiteUserUsernameChanger siteUserUsernameChanger){
+    public void changeUsername(@RequestBody SiteUserUsernameChanger siteUserUsernameChanger) {
         SiteUser tempUser = siteUserRepository.findUserById(siteUserUsernameChanger.getUserId());
 
         tempUser.setUsername(siteUserUsernameChanger.getNewName());
@@ -90,14 +60,48 @@ public class SiteUserController {
     }
 
     @PostMapping(value = "/users/register", consumes = "application/json")
-    public void registerUser(@RequestBody SiteUserRegisterer registerUser){
-        SiteUser user = new SiteUser(registerUser.getUsername(), registerUser.getPassword(),registerUser.geteMail());
+    public String registerUser(@RequestBody SiteUserRegisterer registerUser) {
 
-        siteUserRepository.save(user);
+        if (!(registerUser.getUsername().isBlank() || registerUser.getPassword().isBlank() ||
+            registerUser.geteMail().isBlank())) {
+
+            SiteUser user = new SiteUser(registerUser.getUsername(), registerUser.getPassword(),
+                registerUser.geteMail()); //TODO add pw hashing to this
+
+            siteUserRepository.save(user);
+            return "Sucessfull registration.";
+        }
+        return "Error, username, password, or email cannot be empty.";
     }
 
-    @GetMapping(value = "/users/{userId}/password")
-    public void testMethod(@PathVariable Long userId){
-        System.out.println(siteUserRepository.findUserById(userId).getPassword());
+    @PutMapping(value = "/users/{userId}/password", consumes = "application/json")
+    public String changePassword(@PathVariable Long userId, @RequestBody SiteUserPasswordChangerPojo passwords) {
+        SiteUser currentUser = siteUserRepository.findUserById(userId);
+
+        System.out.println(currentUser.getPassword());
+        String out;
+        if (currentUser.getPassword().equals(passwords.getOldPassword())) //TODO add a hashing to this
+        {
+            currentUser.setPassword(passwords.getNewPassword());
+            siteUserRepository.save(currentUser);
+            out = "Successfully changed password.";
+        } else {
+            out = "Error, mismatching passwords.";
+        }
+        System.out.println(currentUser.getPassword());
+
+        return out;
+    }
+
+    @DeleteMapping(value = "/users/{userId}/delete")
+    public String deleteAccount(@PathVariable Long userId, @RequestBody Map<String, String> json) {
+
+        SiteUser currentUser = siteUserRepository.findUserById(userId);
+
+        if (currentUser.getPassword().equals(json.get("password"))) { //TODO ADD HASHING TO THIS
+            siteUserRepository.delete(currentUser);
+            return "User deleted.";
+        }
+        return "Mismatching passwords.";
     }
 }
