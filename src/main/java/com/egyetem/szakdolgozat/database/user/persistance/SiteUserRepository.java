e61c88ddddcb0d0a17933af060d1b643abcfef0f
Errelian <55238385+Errelian@users.noreply.github.com>
2022-03-03 13:46:32 +0100

    Set env.

--- src/main/java/com/egyetem/szakdolgozat/database/user/persistance/SiteUserRepository.java
+++ src/main/java/com/egyetem/szakdolgozat/database/user/persistance/SiteUserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface SiteUserRepository extends JpaRepository<SiteUser, Integer> {
+public interface SiteUserRepository extends JpaRepository<SiteUser, Long> {
 
     Optional<SiteUser> findUserById(Long id);
 
